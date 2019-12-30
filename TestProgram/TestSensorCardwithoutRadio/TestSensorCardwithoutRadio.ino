e1c576fd7ed43f591b4e2939bf94185969a6a545
ROBLOT Brice <brice.roblot@gmail.com>
2019-12-30 17:32:26 +0100

    Remake of sensor card programm

--- TestProgram/TestSensorCardwithoutRadio/TestSensorCardwithoutRadio.ino
+++ TestProgram/TestSensorCardwithoutRadio/TestSensorCardwithoutRadio.ino
@@ -1,25 +1,28 @@
-
 //Ajout des librairies
-
-
 #include <SPI.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <RFM69.h>
+#include <RFM69registers.h>
+#include <RFM69_ATC.h>
+#include <RFM69_OTA.h>
 
 
 //Definition des variables
-int NbrePluie = 0; //compteur pour le nombre de basculement depuis le dernier envoie
-int DirectionVent; // entier de 0 a 1023
-int VitesseVent; //envoie la frequence de rotation de l'anenometre
+float RainGauge = 0; //level of water fell, in mm
+int WindDirection; // entier de 0 a 360
+float WindSpeed; //envoie la frequence de rotation de l'anenometre
 int Temp; // variable pour la température
 
 //Variable volatile pour les interrupt
-int ListeIntervalVitesse[20];
-int IndiceVistesseVent = 0;
-int LastMillis = 0;
+volatile long LastWindSpeed = 0;
+volatile int WindSpeedClick = 0;
+long LastWindCheck = 0;
 
 // Variables pour le comptage pluviométrique
 int LastButtonState = LOW;
 unsigned long LastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 0;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 10;    // the debounce time; increase if the output flickers
 
 // Variables pour l'envoie des données
 bool EnvoieData = false;
@@ -28,97 +31,85 @@ int LenMessageData;
 
 //Definition des Pins des capteurs
 
-const int PinTemp = A3;
-const int PinPluie = 6;
-const int PinVitesse = 3;
-const int PinDirection = A3;
+const byte PinTemp = 7;
+const byte PinPluie = 6;
+const byte PinVitesse = 3;
+const byte PinDirection = A3;
 
 unsigned long LastAffichage;
 
+// init of temp sensor with oneWire communication
+OneWire oneWire(PinTemp);
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensorDeviceAddress;
 
-//interrupt vitesse vent
-void VitesseVentInterrupt() {
-  int t = millis();
-  ListeIntervalVitesse[IndiceVistesseVent] = t - LastMillis;
-  LastMillis = t;
-  IndiceVistesseVent = (IndiceVistesseVent + 1) % 20;
 
+//interrupt wind speed
+void WindSpeedInterrupt()
+{
+  if ((millis() - LastWindSpeed) > 10)
+  {
+    WindSpeedClick++;
+    LastWindSpeed = millis();
+  }
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  //Interrupt for wind speed
+  attachInterrupt(digitalPinToInterrupt(PinVitesse), WindSpeedInterrupt, RISING);
+  interrupts(); //turn on the interrrupt for wind speed
 
-
-  //Interrupt pour gérer les capteur :
-  attachInterrupt(digitalPinToInterrupt(PinVitesse), VitesseVentInterrupt, RISING);
-
-  //Pour le capteur de pluie
+  //for rain gauge
   pinMode(PinPluie, INPUT);
 
+  //for temperature
+  sensors.begin();
+  sensors.getAddress(sensorDeviceAddress, 0);
+  sensors.setResolution(sensorDeviceAddress, 9);
+
   Serial.begin(9600);
-
-  LastAffichage = millis()/1000;
+  LastAffichage = millis() / 1000;
 }
 
 
-
-float FrequenceVent(int ListeTempo[], int LenListe){
-  float Frequence;
-  int sum;
-
-  for (int i = 0; i < LenListe; i++){
-    sum += ListeTempo[i]; 
-  }
-  Frequence = 1000/(sum/LenListe);
-}
-
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-
+void loop()
+{
   //Lecture du capteur de pluvio
   int ReadingPluie = digitalRead(PinPluie);
-  if (ReadingPluie != LastButtonState) {// reset the debouncing timer
+  if (ReadingPluie != LastButtonState)
+  { // reset the debouncing timer
     LastDebounceTime = millis();
   }
-  if ((millis() - LastDebounceTime) > debounceDelay) {
+  if ((millis() - LastDebounceTime) > debounceDelay)
+  {
     // whatever the ReadingPluie is at, it's been there for longer than the debounce
     // delay, so take it as the actual current state:
     // if the button state has changed:
-    if (ReadingPluie != LastButtonState) {
-      NbrePluie += 1; //Il y a eu comptage d'un versement du godet.
+    if ((ReadingPluie != LastButtonState) && (LastButtonState == LOW)) {
+      RainGauge += 0.2794; //Il y a eu comptage d'un versement du godet.
       LastButtonState = ReadingPluie;
     }
   }
 
-
-
   //###############################
 
-  if(((millis()/1000 % 5) == 0) && (LastAffichage != (millis()/1000))){
+  if (((millis() / 1000 % 5) == 0) && (LastAffichage != (millis() / 1000)))
+  {
     //Va à la recherche des données
-    VitesseVent = FrequenceVent(ListeIntervalVitesse, 20);
-    DirectionVent = analogRead(PinDirection);
-    Temp = 0;
+    WindSpeed = getWindSpeed();
+    WindDirection = getWindDirection();
+    Temp = getTemperature();
 
     /* Affichage */
     Serial.print("Frequence Vent :  ");
-    float Frequence;
-    int sum = 0;
-
-    for (int i = 0; i < 20; i++){
-      sum += ListeIntervalVitesse[i]; 
-    }
-    Frequence = 1000/(sum/20);
-    Serial.println(1000/(sum/20));
+    Serial.println(WindSpeed);
 
     Serial.print("Direction du vent :  ");
-    Serial.println(DirectionVent);
+    Serial.println(WindDirection);
 
-    Serial.print("Nombre de basculement pluviometre :  ");
-    Serial.println(NbrePluie);
+    Serial.print("Nombre de mm tombé :  ");
+    Serial.println(RainGauge);
 
     Serial.print("Temperature :  ");
     Serial.println(Temp);
@@ -126,20 +117,63 @@ void loop() {
     Serial.println("");
     Serial.println("****************************************************");
     Serial.println("");
-    
+
 
     /*
-    for(int i=0; i<20; i++){
+      for(int i=0; i<20; i++){
       Serial.print(ListeIntervalVitesse[i]);
       Serial.print("; ");
-    }
-    Serial.println("");
+      }
+      Serial.println("");
 
-    LastAffichage = millis()/1000;
+      LastAffichage = millis()/1000;
     */
-    
-    
-  }
- 
 
+
+  }
+}
+
+float getWindSpeed() {
+
+  //Return the speed of the wind
+  float deltaTime = millis() - LastWindCheck; //time between two check of wind speed (always < 3min)
+  deltaTime /= 1000.0; //convert to s
+
+  float WindSpeed = float(WindSpeedClick) / deltaTime; //frequency of click
+  WindSpeedClick = 0; //init the counter
+  LastWindCheck = millis();
+  WindSpeed *= 2.4;
+
+  return (WindSpeed);
+}
+
+int getWindDirection()
+{
+  //return the angle forme between the wind and north (north = 0°)
+  int WindAnalog = analogRead(PinDirection);
+  
+  if (WindAnalog < 75) return(113);
+  if (WindAnalog < 88) return(68);
+  if (WindAnalog < 109) return(90);
+  if (WindAnalog < 155) return(158);
+  if (WindAnalog < 214) return(135);
+  if (WindAnalog < 265) return(203);
+  if (WindAnalog < 346) return(180);
+  if (WindAnalog < 433) return(23);
+  if (WindAnalog < 530) return(45);
+  if (WindAnalog < 615) return(248);
+  if (WindAnalog < 666) return(225);
+  if (WindAnalog < 744) return(338);
+  if (WindAnalog < 806) return(0);
+  if (WindAnalog < 856) return(293);
+  if (WindAnalog < 916) return(315);
+  if (WindAnalog < 984) return(270);
+}
+
+float getTemperature()
+{
+  //get the temperature of the Temp sensor
+  //Only ask for the sensor on index 0 of the OneWire Bus
+  sensors.requestTemperatures();
+  Temp = sensors.getTempCByIndex(0);
 }
